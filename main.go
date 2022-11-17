9246ff2069dd7b70f8308c3273c4a0437bb63047
dbyqs21 <dbyqs21@126.com>
2022-11-17 09:59:10 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	if err != nil {
+		panic(err)
+	}
+
+}
